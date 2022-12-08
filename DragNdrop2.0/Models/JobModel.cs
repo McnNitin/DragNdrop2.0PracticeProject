bdc08320f636f60917b8487d0fff3ebc0cfb4803
Nitin0386346 <56183506+Nitin0386346@users.noreply.github.com>
2022-12-08 11:36:09 +0530

    Add project files.

---
+++ DragNdrop2.0/Models/JobModel.cs
@@ -0,0 +1,26 @@
+﻿namespace DragNdrop2._0.Models
+{
+    public class JobModel
+    {
+        public int Id { get; set; }
+        public JobStatuses Status { get; set; }
+        public string Description { get; set; }
+        public DateTime LastUpdated { get; set; }
+    }
+
+    public enum JobStatuses
+    {
+        //Todo,
+        //Started,
+        //InProgress,
+        //Completed
+        //Done
+        
+        TODO,
+        INPROGRESS,
+        COMPLETED,
+        DONE
+
+
+    }
+}
